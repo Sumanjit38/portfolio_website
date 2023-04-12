23826194f80b588ff484f2f1fd5b55de949813df
Sumanjit38 <sumanjit38@gmail.com>
2023-04-12 21:38:52 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import Navbar from './Navbar'
+import Slider from './components/Slider'
 function App() {
   return (
     <>
       <Navbar />
+      <Slider />
     </>
   )
 }
