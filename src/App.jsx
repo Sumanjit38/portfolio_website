f44e95e5999fe008450dd55f12da179d608cbdcc
Sumanjit38 <sumanjit38@gmail.com>
2023-04-23 00:34:20 +0530

    create app

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import Header from './components/header/Header'
+import Navbar from './components/navBar/Navbar'
+import About from './components/about/About'
+import Skills from './components/skills/Skills'
+import Projects from './components/projects/Projects'
+import Academics from './components/academics/Academics'
+import Contact from './components/contact/Contact'
+import Footer from './components/footer/Footer'
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Navbar />
+      <About />
+      <Skills />
+      <Projects />
+      <Academics />
+      <Contact />
+      <Footer />
+    </>
+  )
+}
+
+export default App
