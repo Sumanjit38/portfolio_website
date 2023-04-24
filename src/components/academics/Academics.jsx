afc2b98793936c0774b7c2e271436abbfeef7d3a
Sumanjit38 <sumanjit38@gmail.com>
2023-04-24 13:06:46 +0530

    first update

--- src/components/academics/Academics.jsx
+++ src/components/academics/Academics.jsx
@@ -3,7 +3,17 @@ import './academics.css'
 
 const Academics = () => {
   return (
-    <section id = 'academics'>Academics</section>
+    <section id = 'academics'>
+      <h5>My Academic Details</h5>
+      <h2>Qualifications</h2>
+
+      <div className='container academic__container'>
+        <article className='qualifications'>
+          
+        </article>
+      </div>
+
+    </section>
   )
 }
 export default Academics;
