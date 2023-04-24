d7e6bcc6302d71556e75a8309ba3d917d5e288ac
Sumanjit38 <sumanjit38@gmail.com>
2023-04-24 13:07:41 +0530

    first update

--- src/components/navBar/Navbar.jsx
+++ src/components/navBar/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './navbar.css';
 import {AiFillHome} from 'react-icons/ai'
 import {FaUserAlt} from 'react-icons/fa'
-//import {FaUserAlt} from 'react-icons/fa'
+import {SiSkillshare} from 'react-icons/si'
 import {HiFolderOpen} from 'react-icons/hi'
 import {HiAcademicCap} from 'react-icons/hi'
 import {FcContacts} from 'react-icons/fc'
@@ -14,7 +14,7 @@ const Navbar = () => {
     <nav>
       <a href='#' onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}><AiFillHome /></a>
       <a href='#about' onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><FaUserAlt /></a>
-      <a href='#' onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><AiFillHome/></a>
+      <a href='#skills' onClick={() => setActiveNav('#skills')} className={activeNav === '#skills' ? 'active' : ''}><SiSkillshare/></a>
       <a href='#projects' onClick={() => setActiveNav('#projects')} className={activeNav === '#projects' ? 'active' : ''}><HiFolderOpen/></a>
       <a href='#academics' onClick={() => setActiveNav('#academics')} className={activeNav === '#academics' ? 'active' : ''}><HiAcademicCap/></a>
       <a href='#contacts' onClick={() => setActiveNav('#contacts')} className={activeNav === '#contacts' ? 'active' : ''}><FcContacts/></a>
